9dda9174bcc029f2f4c5f212bd128cc39323dd45
Aivan Monceller <aivan@monceller.com>
2026-02-24 21:02:14 +0100

    fix: make collisions more forgiving

--- src/entities/Enemy.ts
+++ src/entities/Enemy.ts
@@ -1,12 +1,23 @@
-﻿import Phaser from 'phaser';
+import Phaser from 'phaser';
 import Player from './Player';
+
+function insetRect(rect: Phaser.Geom.Rectangle, insetXPct: number, insetYPct: number) {
+  const insetX = rect.width * insetXPct;
+  const insetY = rect.height * insetYPct;
+  return new Phaser.Geom.Rectangle(
+    rect.x + insetX / 2,
+    rect.y + insetY / 2,
+    rect.width - insetX,
+    rect.height - insetY
+  );
+}
+
 export default class Enemy extends Phaser.GameObjects.Sprite {
   speed: number;
   minY: number;
   maxY: number;
   minSpeed: number;
   maxSpeed: number;
-  direction: number;
   player: Player;
 
   constructor(scene: Phaser.Scene, x: number, y: number) {
@@ -16,12 +27,14 @@ export default class Enemy extends Phaser.GameObjects.Sprite {
     this.setFlipX(true);
     this.minY = 80;
     this.maxY = 280;
+
     // pixels per second
     this.minSpeed = 120;
     this.maxSpeed = 270;
-    this.direction = Math.random() < 0.5 ? 1 : -1;
-    this.speed =
-      this.minSpeed + Math.random() * (this.maxSpeed - this.minSpeed);
+    this.speed = this.minSpeed + Math.random() * (this.maxSpeed - this.minSpeed);
+
+    // randomize direction per enemy so patterns feel less uniform
+    if (Math.random() < 0.5) this.speed *= -1;
   }
 
   setPlayer(player: Player) {
@@ -43,8 +56,10 @@ export default class Enemy extends Phaser.GameObjects.Sprite {
       this.speed *= -1;
     }
 
-    let playerRect = this.player.getBounds();
-    let enemyRect = this.getBounds();
+    // Slightly shrink hitboxes so collisions feel fairer than sprite bounds.
+    const playerRect = insetRect(this.player.getBounds(), 0.2, 0.25);
+    const enemyRect = insetRect(this.getBounds(), 0.28, 0.35);
+
     if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, enemyRect)) {
       return this.emit('kill');
     }
